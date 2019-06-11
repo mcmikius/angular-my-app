b16b81dd3f7fb2e2082cf09317d3c1d1d95c2d89
Michail Bondarenko <mcmikius15@gmail.com>
2019-06-11 08:12:15 +0300

    first component

--- angular-my-app/src/app/app.module.ts
+++ angular-my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MynewcomponentComponent } from './mynewcomponent/mynewcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MynewcomponentComponent
   ],
   imports: [
     BrowserModule,
